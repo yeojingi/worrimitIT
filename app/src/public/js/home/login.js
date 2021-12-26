82fe38172a1a067bf262e017ab5c78db40b114d6
yeojingi <businessjingi@gmail.com>
2021-12-26 13:23:43 +0900

    HTML object is manipulated by DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 "use strict";
 
-console.log('hello');
+const id        = document.querySelector('#id'),
+    password    = document.querySelector('#password'),
+    loginBtn    = document.querySelector('button');
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        password: password.value,
+    };
+
+    console.log(req);
+}
